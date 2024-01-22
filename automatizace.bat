96794fc003fd1f94e8279c4640f30b752a352461
Your Name <you@example.com>
2024-01-22 11:27:25 +0100

    d

--- automatizace.bat
+++ automatizace.bat
@@ -1,8 +1,19 @@
 @echo off 
 
+setlocal enabledelayedexpansion
+
 param(
     [string]$file
     [string]$pswd
 )
 
-if (-not $file)
+if (-not $file) {
+    Write-Host "chybi povinny parametr -file"
+    exit
+}
+
+if(-not $pswd) {
+    $pswd = "defaultheslo"
+}
+
+$username = get-content

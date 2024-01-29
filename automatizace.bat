4a9bdb7de73f1f3a071535ccf9e771890c6cb43c
Your Name <you@example.com>
2024-01-29 11:05:04 +0100

    autt

--- automatizace.bat
+++ automatizace.bat
@@ -4,15 +4,19 @@ setlocal enabledelayedexpansion
  
 set "filePath=R:\osy_au"
 set "pswd="
-set "gr1="
  
-if not defined password set "password=Default"
+if (not defined password set "password=Default"
+)
  
+net localgroup #gr1# /add 
+net localgroup #gr2#  /add
+
 for /f "delims=" %%a in (%filePath%) do (
 echo Adding user: "%%a" with password : Ipassword!
 net user "%%a" !password! /add
+net localgroup #gr1# "%%a" /add 
+net localgroup #gr2#  "%%a" /add
 )
 
-net localgroup #gr1# /add 
-net localgroup #gr1# uživ /add 
-net localgroup #gr1#  
+pause 
+echo

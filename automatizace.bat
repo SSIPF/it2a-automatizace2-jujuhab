1f5e3ef8b6c81a7214a2d19c70597014abd2dc26
Your Name <you@example.com>
2024-01-29 10:21:06 +0100

    aut

--- automatizace.bat
+++ automatizace.bat
@@ -1,19 +1,13 @@
-@echo off 
-
+@echo off
+ 
 setlocal enabledelayedexpansion
-
-param(
-    [string]$file
-    [string]$pswd
+ 
+set "filePath=C: \Users\sysadmin\Downloads\osyc\userlist\user_list.txt"
+set "pswd="
+ 
+if not defined password set "password=Default"
+ 
+for /f "delims=" %%a in (%filePath%) do (
+echo Adding user: "%%a" with password : Ipassword!
+net user "%%a" !password! /add
 )
-
-if (-not $file) {
-    Write-Host "chybi povinny parametr -file"
-    exit
-}
-
-if(-not $pswd) {
-    $pswd = "defaultheslo"
-}
-
-$username = get-content

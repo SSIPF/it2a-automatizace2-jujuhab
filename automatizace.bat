809a89c4aec818446b0809f33d285e365d6b78f3
Your Name <you@example.com>
2024-02-12 11:01:16 +0100

    cc

--- automatizace.bat
+++ automatizace.bat
@@ -8,13 +8,12 @@ set "group="
  
 if (not defined password set "password=Default"
 )
- 
-net localgroup "gr1" /add 
-net localgroup "gr2" /add
 
 for /f "delims=" %%a in (%filePath%) do (
 echo Adding user: "%%a" with password : !password!
 net user "%%a" !password! /add
+
+
 net localgroup "gr1" "%%a" /add 
 net localgroup "gr2"  "%%a" /add
 )
